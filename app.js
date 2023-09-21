02195d61bf5279a58e1887e635c7adc8785d6232
Rafał Chojnacki <r.chojnacki2@o2.pl>
2023-09-21 21:47:18 +0200

    rest api

--- app.js
+++ app.js
@@ -1,84 +1,25 @@
-const express = require('express');
-const logger = require('morgan');
-const cors = require('cors');
+import express, { json } from 'express';
+import logger from 'morgan';
+import cors from 'cors';
+
+import contactsRouter from './routes/api/contacts.js';
+
 const app = express();
-const Joi = require('joi'); // Importuj pakiet Joi
+
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 
 app.use(logger('dev'));
-app.use(express.json());
+app.use(json());
 app.use(cors());
 
-// Import funkcji obsługujących operacje na kontaktach
-const {
-  listContacts,
-  getById,
-  addContact,
-  removeContact,
-  updateContact,
-} = require('./contactsController');
+app.use('/api/contacts', contactsRouter);
 
-// Definicje walidacji dla danych wejściowych (POST i PUT)
-const contactSchema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().email().required(),
-  phone: Joi.string().required(),
+app.use((req, res) => {
+	res.status(404).json({ message: 'Not found' });
 });
 
-// Endpoint do pobierania listy kontaktów
-app.get('/api/contacts', (req, res) => {
-  const contacts = listContacts();
-  res.status(200).json(contacts);
+app.use((err, req, res, next) => {
+	res.status(500).json({ message: err.message });
 });
 
-// Endpoint do pobierania kontaktu po ID
-app.get('/api/contacts/:id', (req, res) => {
-  const contact = getById(req.params.id);
-  if (contact) {
-    res.status(200).json(contact);
-  } else {
-    res.status(404).json({ message: 'Not found' });
-  }
-});
-
-// Endpoint do dodawania nowego kontaktu z walidacją
-app.post('/api/contacts', (req, res) => {
-  const { error } = contactSchema.validate(req.body);
-  if (error) {
-    return res.status(400).json({ message: error.details[0].message });
-  }
-
-  const newContact = addContact({ ...req.body });
-  res.status(201).json(newContact);
-});
-
-// Endpoint do usuwania kontaktu po ID
-app.delete('/api/contacts/:id', (req, res) => {
-  const contactId = req.params.id;
-  const result = removeContact(contactId);
-  if (result) {
-    res.status(200).json({ message: 'Contact deleted' });
-  } else {
-    res.status(404).json({ message: 'Not found' });
-  }
-});
-
-// Endpoint do aktualizacji kontaktu po ID z walidacją
-app.put('/api/contacts/:id', (req, res) => {
-  const { error } = contactSchema.validate(req.body);
-  if (error) {
-    return res.status(400).json({ message: error.details[0].message });
-  }
-
-  const contactId = req.params.id;
-  const updatedContact = updateContact(contactId, { ...req.body });
-  if (updatedContact) {
-    res.status(200).json(updatedContact);
-  } else {
-    res.status(404).json({ message: 'Not found' });
-  }
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+export default app;
